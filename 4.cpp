254744e8e0a2786f4a549147fd9230cef9820bc1
Saitelz <57504388+Saitelz@users.noreply.github.com>
2020-01-12 18:09:26 +0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,45 @@
+﻿//Напишите эффективную процедуру, выписывающую из массива длины n все элементы, у которых произведение ненулевых цифр больше k. В комментариях напишите, почему перебор - эффективный
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	long long n, k;
+	cin >> n;
+	vector <long long> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	cin >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int t = arr[i];
+		int h = 1;
+		for (int j = 0; t; j++)
+		{
+			if (t % 10 != 0)
+			{
+				h *= t % 10;
+			}
+			t /= 10;
+		}
+		if (h > k)
+		{
+			if (arr[i] == 0 && arr[i] > k)
+			{
+				cout << arr[i] << " ";
+			}
+			else if (arr[i] != 0)
+			{
+				cout << arr[i] << " ";
+			}
+		}
+	}
+}
+/*перебор эффективный, т.к. работает за O(n * log m),
+ где m - средняя длина чисел*/
